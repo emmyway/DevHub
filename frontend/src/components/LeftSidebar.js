298cd4c060baa11a8e43b59deebb9a4577b82666
Emmanuel Nnaji <realreaach@yahoo.com>
2024-10-18 12:58:19 +0100

    added images

--- frontend/src/components/LeftSidebar.js
+++ frontend/src/components/LeftSidebar.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
+import calmlyImage from '../images/rest.jpg';
 
 const LeftSidebar = () => {
   return (
     <Col xs={12} md={3} className="bg-light">
-      <h4>Left Sidebar</h4>
+      <h4>Growth</h4>
       {/* Add content here, e.g., tags, categories, etc. */}
       <ul>
-        <li><a href="#">Tag 1</a></li>
-        <li><a href="#">Tag 2</a></li>
-        <li><a href="#">Tag 3</a></li>
+        <li>
+          <a href="#">Dev Community</a>
+          <p>DEV Community is a community of 2,198,981 amazing developers
+          We're a place where coders share, stay up-to-date and grow their careers.</p>
+        </li>
+        <li>
+          <a href="#">#discussion</a>
+          <p>Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). Where does it come from? Contrary to popular belief</p>
+        </li>
+        <img src={ calmlyImage } alt="ai img" className="container-fluid"/>
+        <li><a href="#">#region</a></li>
       </ul>
     </Col>
   );
--- frontend/src/components/RightSidebar.js
+++ frontend/src/components/RightSidebar.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
+import officeImage from '../images/office.jpg';
+import noteImage from '../images/note.jpg';
 
 const RightSidebar = () => {
   return (
     <Col xs={12} md={3} className="bg-light">
-      <h4>Right Sidebar</h4>
+      <h4>Trending</h4>
       {/* Add content here, e.g., user info, trending articles, etc. */}
-      <p>User Profile</p>
+      <p>Portfolio</p>
       <ul>
-        <li><a href="#">Trending Article 1</a></li>
-        <li><a href="#">Trending Article 2</a></li>
+        <li>
+          <a href="#">Trending Article 1</a></li>
+          <img src={officeImage} alt="office" className="container-fluid"/>
+        <li>
+          <a href="#">Trending Article 2</a></li>
+          <img src={ noteImage } alt="ai img" className="container-fluid"/>
         <li><a href="#">Trending Article 3</a></li>
       </ul>
     </Col>
