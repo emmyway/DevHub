a9eea30ab698b2930ef24238d1c1ce63a0d858da
Olanrewaju Awoyemi <olanrewajuawoyemi@gmail.com>
2024-10-24 05:01:57 +0100

    update config models requirements

--- backend/config.py
+++ backend/config.py
@@ -4,12 +4,20 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_jwt_extended import JWTManager
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
-app.config["JWT_SECRET_KEY"] = os.environ.get(
-    "JWT_SECRET_KEY", "your-secret-key")  # Change this to a secure secret key
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 3600  # 1 hour
+app.config["UPLOAD_FOLDER"] = os.path.join(os.path.dirname(os.path.abspath(__file__)), "uploads")
+app.config["ALLOWED_EXTENSIONS"] = {'png', 'jpg', 'jpeg', 'gif'}
+if not os.path.exists(app.config["UPLOAD_FOLDER"]):
+    os.makedirs(app.config["UPLOAD_FOLDER"])
+
+app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY", "your-secret-key")  # Change this to a secure secret key
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 399600  # 1 hour
+
+CORS(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
